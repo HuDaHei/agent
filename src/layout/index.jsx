b413e88b5621cc7c489eaff30ccc21231da450fc
韩军 <>
2020-08-29 10:08:34 +0800

    添加路由导航

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,3 +1,5 @@
+import "./index.scss";
+import navLogo from "@/assets/navLogo.svg";
 const layoutMenu = {
   name: "layoutNav",
   computed: {
@@ -19,17 +21,26 @@ const layoutMenu = {
     // const firstLevelMenu = this.createMenu(this.routes);
     return (
       <section>
-        <section>
-          <nav>
-            {this.routes.map(r => {
-              const { path, meta = {} } = r;
-              const { menuName } = meta;
-              const to = { path };
-              return <router-link to={to}>{menuName}</router-link>;
-            })}
-          </nav>
+        <section class="first_nav_container">
+          <h4 class="first_nav_sign">
+            <img src={navLogo} width="84" height="42" />
+            <span class="sign_title">代理商服务系统</span>
+          </h4>
+          <section>
+            <nav>
+              <el-menu mode="horizontal">
+                {this.routes.map(r => {
+                  const { path, meta = {} } = r;
+                  const { menuName } = meta;
+                  return <el-menu-item index={path}>{menuName}</el-menu-item>;
+                })}
+              </el-menu>
+            </nav>
+          </section>
         </section>
-        <router-view></router-view>
+        <section>
+          <router-view></router-view>
+        </section>
       </section>
     );
   }
--- src/utils/loadElementUI.js
+++ src/utils/loadElementUI.js
@@ -1,8 +1,10 @@
-import { Input, Form, FormItem, Button } from "element-ui";
+import { Input, Form, FormItem, Button, Menu, MenuItem } from "element-ui";
 export function loadElementUI(Vue) {
   Vue.prototype.$ELEMENT = { size: "small", zIndex: 3000 };
   Vue.use(Input);
   Vue.use(Form);
   Vue.use(FormItem);
   Vue.use(Button);
+  Vue.use(Menu);
+  Vue.use(MenuItem);
 }
