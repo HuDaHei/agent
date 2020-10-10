28703bace67abc7230ca1ef36f0cfbd6a63f83c2
DESKTOP-A8FIUDD\兜里 <1305544590@qq.com>
2020-10-10 16:34:09 +0800

    css调整

--- src/components/permission-router-view/index.vue
+++ src/components/permission-router-view/index.vue
@@ -1,26 +1,27 @@
 <template>
-    <div class="permission_router_view">
-        <template v-if="$_hasPermisson(permissionCode)">
-            <router-view></router-view>
-        </template>
-        <template v-else>
-            <div>暂无权限</div>
-        </template>
-    </div>
+  <div class="permission_router_view">
+    <template v-if="$_hasPermisson(permissionCode)">
+      <router-view></router-view>
+    </template>
+    <template v-else>
+      <div>暂无权限</div>
+    </template>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'permissionRouterView',
-    props: {
-        permissionCode: {
-            type:String,
-            required:true
-        }
-    }
-}
+  name: "permissionRouterView",
+  props: {
+    permissionCode: {
+      type: String,
+      required: true,
+    },
+  },
+};
 </script>
-
-<style>
-
+<style lang="scss" scoped>
+.permission_router_view {
+  height: 100%;
+}
 </style>
